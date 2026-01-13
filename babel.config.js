1db93cbc1d3980ad9937ebb3660a7a11c950e80d
anirudhqwerty <therealanirudhsharma@gmail.com>
2026-01-13 22:58:23 +0530

    fixing bugs

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // Required for Expo Router
-      'expo-router/babel',
-      // Required for animations (if you use Reanimated)
+      // 'expo-router/babel' is deprecated and NOT needed here anymore
       'react-native-reanimated/plugin',
     ],
   };
